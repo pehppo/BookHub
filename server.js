04bede15070781181e93328846349534bd0e3a59
KaueLS <kaue.lsantos.dev@gmail.com>
2026-06-03 17:12:43 -0300

    modified port

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ app.get('/books/:id', async (req, res) => {
         });
     }
 });
-app.listen(3000, () => {
-    console.log('Servidor rodando');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
