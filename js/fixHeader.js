f9a186f58bdc6ab8cb4bae1492693b41d6c5dc38
KaueLS <kaue.lsantos.dev@gmail.com>
2026-06-05 17:25:31 -0300

    hotfix: fixHeader

--- js/fixHeader.js
+++ js/fixHeader.js
@@ -1,7 +1,12 @@
 window.addEventListener('scroll', () => {
     const header = document.querySelector('.header');
 
-    if (window.scrollY > 100 && window.innerHeight < 700) {
+    const isMobile = window.innerWidth <= 768;
+
+    if (
+        (isMobile && window.scrollY > 100 && window.innerHeight < 700) ||
+        (!isMobile && window.scrollY > 100)
+    ) {
         header.classList.add('sticky');
     } else {
         header.classList.remove('sticky');
